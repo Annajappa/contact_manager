07993adc1865a6a4d059ee13ed7defa60e91fdb0
Gadige Annajappa Gadige Meghanath <g.meghanath@worktual.com>
2025-10-06 19:13:20 +0530

    Ignore the .env file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import fs from 'fs';
+import cors from 'cors';
 
 
 
@@ -10,6 +11,7 @@ let port = process.env.PORT;
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 
 
